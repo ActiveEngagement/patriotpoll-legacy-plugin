6dd8f9bae067825f372a56d532700f690af857d2
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro.local>
2020-11-11 18:37:52 -0700

    Fixed issue with loading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-export * from './Components/Poll';
+import Poll from './Components/Poll/Poll';
+import PollArchive from './Components/Poll/PollArchive';
+import PollCard from './Components/Poll/PollCard';
+import PollLoader from './Components/Poll/PollLoader';
+import PollResults from './Components/Poll/PollResults';
+
+export {
+    Poll,
+    PollArchive,
+    PollCard,
+    PollLoader,
+    PollResults,
+};
