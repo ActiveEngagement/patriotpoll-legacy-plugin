134326e42a4bfb7f0ebca29d59fd0df83dff2f0c
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro.local>
2020-11-11 18:45:06 -0700

    More updates to index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import Poll from './Components/Poll/Poll';
 import PollArchive from './Components/Poll/PollArchive';
-import PollCard from './Components/Poll/PollCard';
 import PollLoader from './Components/Poll/PollLoader';
-import PollResults from './Components/Poll/PollResults';
 
 export {
-    Poll,
     PollArchive,
-    PollCard,
     PollLoader,
-    PollResults,
 };
