3b649e63e4d5a7ef5b92a2c888154013700482e1
Justin Kimbrell <jkimbrell@actengage.com>
2020-12-10 19:10:27 -0700

    pre-release commit

--- src/Components/Poll/PollQuestion.vue
+++ src/Components/Poll/PollQuestion.vue
@@ -79,7 +79,7 @@ export default {
         PollEmbed,
         // AnimateCss,
         FontAwesomeIcon,
-        PollSocialButtons: () => import(/* webpackChunkName: 'poll-social-buttons', webpackPreload: true */'./PollSocialButtons')
+        PollSocialButtons: () => import(/* webpackChunkName: 'poll-social-buttons', webpackPrefetch: true */'./PollSocialButtons')
     },
 
     directives: {
