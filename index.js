fd03df17b553c51594ca95ce1b0eb707f93d4fa6
Hostinger <hello@mntarts.org>
2020-12-15 13:57:43 -0700

    More build changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import PollArchive from './src/Components/Poll/PollArchive';
+import PollEmbed from './src/Components/Poll/PollEmbed';
 import PollLoader from './src/Components/Poll/PollLoader';
 import PatriotPollPlugin from './src/Plugins/PatriotPollPlugin';
 
 export {
     PollArchive,
+    PollEmbed,
     PollLoader,
     PatriotPollPlugin
 };
