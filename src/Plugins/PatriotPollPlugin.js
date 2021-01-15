5087b7a1722797341cb9ea93a8c4fbcaa34657ba
Justin Kimbrell <jkimbrell@actengage.com>
2021-01-14 20:38:19 -0700

    Fixed issue if install the plugin without axios installed

--- src/Plugins/PatriotPollPlugin.js
+++ src/Plugins/PatriotPollPlugin.js
@@ -3,13 +3,21 @@ import Axios from 'axios';
 export const BASE_URL = 'https://api.patriotpoll.net/v1';
 
 export default function(Vue, options = {}) {
+    // If Axios is not installed, then just ignore the plugin.
+    if(!Axios) {
+        return;
+    }
+
+    // Get the api key from the options or config file.
     const apiKey = options.apiKey || process.env.VUE_APP_PATRIOT_POLL_API_KEY;
 
+    // Gethe base url from the options or config file.
     const baseURL = options.baseUrl
         || options.baseURL
         || process.env.VUE_APP_PATRIOT_POLL_BASE_URL
         || BASE_URL;
     
+    // Create an Axios instance.
     const axios = Axios.create({
         baseURL,
         headers: {
@@ -17,6 +25,7 @@ export default function(Vue, options = {}) {
         }
     });
     
+    // Get the data from storage and set the request headers.
     try {
         const store = JSON.parse(window.localStorage.__poll__);
         
@@ -30,5 +39,6 @@ export default function(Vue, options = {}) {
         // Ignore the error
     }
 
+    // Set the axios instace on the prototype.
     Vue.prototype.$patriotpoll = axios;
 }
