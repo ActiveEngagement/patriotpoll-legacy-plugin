adfd2c19eaba67f2a3e225509c138c8311e76838
Justin Kimbrell <jkimbrell@actengage.com>
2021-05-17 13:24:48 -0600

    Updated babel config

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,9 @@ module.exports = {
             corejs: 3,
             useBuiltIns: "usage", // or "entry"
             targets: {
+                ie: 10,
+                ios: 9,
+                safari: 9,
                 browsers: "> .25%, not dead",
             }
         }]
